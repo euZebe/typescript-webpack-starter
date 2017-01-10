f264a0252cf629188dadd0856e9e1fbd87007008
Eusebe <eeesebio@gmail.com>
2017-01-10 21:47:19 +0100

    game implemented, isolating business logic from display logic

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,22 @@
 import { Utils } from "./utils";
+import { GameComponent } from './game-component';
 
+const gameComponent = new GameComponent();
 
-
+const checkButton = <HTMLButtonElement> document.getElementById("checkButton");
 const tryInput = <HTMLInputElement> document.getElementById("try");
-const checkButton = document.getElementById("checkButton");
 
+
+tryInput.onkeypress = function(event) {
+    if (event.keyCode == 13) { // when Enter is pressed
+        gameComponent.checkValue(); // check value
+    }
+}
+
+// gives choice on how to submit value ; press Enter / click button
 checkButton.onclick = function () {
-    console.log("clicked !" + tryInput.value);
+    gameComponent.checkValue();
 };
-
 
 
 // used to be check the displayed version is the current one
